4a8d2209f56b35786f6f66f967334c3eeccc5bd6
inayarajwani <inaya.rajwani@gmail.com>
2024-01-02 18:01:55 -0500

    update form- completed

--- Contact.js
+++ Contact.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 function Contact() {
   const password = 'swordfish';
-  const [authorized, setAuthorized] = useState(state);
+  const [authorized, setAuthorized] = useState(false);
 
   function handleSubmit(e) {
     const enteredPassword = e.target.querySelector(
@@ -10,18 +10,40 @@ function Contact() {
     const auth = enteredPassword == password;
     setAuthorized(auth)
   }
+  //login form
+  const login = (
+    //call handleSubmit function
+    <form action="#" onSubmit={handleSubmit}>
+      <input type="password" placeholder="Password"/>
+      <input type="submit"/>
+    </form>
+    );
 
+  //contact info
+  const contactInfo = (
+    <ul>
+      <li>
+         client@example.com
+      </li>
+      <li>
+         555.555.5555
+      </li>
+    </ul>
+  );
+
+
+  
   return (
       <div id="authorization">
-        <h1>Enter the Password</h1>
-        <ul>
-          <li>
-            client@example.com
-          </li>
-          <li>
-            555.555.5555
-          </li>
-        </ul>
+        <h1>
+        {
+          // if a user hasn't been authorized, display 'Enter the Password'
+          authorized ? "Contact" : "Enter the Password"
+        }
+        </h1>
+        {
+          authorized ? contactInfo : login
+        }
       </div>
   );
 }
